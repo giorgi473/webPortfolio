894d6486d0073cb0835c4321a5db1425cc063bba
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-03-22 20:02:07 +0400

    feat: update Header component scroll  with fixed blue theme and grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0d0218]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#010318ad]`}
       >
         <ThemeProvider
           attribute="class"
